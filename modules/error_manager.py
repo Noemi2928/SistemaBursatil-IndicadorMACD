db9a63286ea8d46aa3ffb3e65fe64d42f8a59cc6
Noemi2928 <126622118+Noemi2928@users.noreply.github.com>
2025-11-02 15:52:32 -0300

    Actualizado error_manager.py

--- modules/error_manager.py
+++ modules/error_manager.py
@@ -7,7 +7,6 @@ class ErrorManager:
             "EXCEL_COLUMN_NULL": "La columna '{column}' del archivo seleccionado no contiene datos.",
             "EXCEL_TRUNCATE": "El archivo Excel contenía más de 20 símbolos. Se truncaron los primeros 20.",
             "VALIDATION_EXCESS_MANUAL": "Se superó el límite de 20 símbolos válidos. Por favor, elimine los símbolos en exceso para continuar: {exceso}.",
-            #"VALIDATION_DUPLICATES": "Se eliminaron símbolos duplicados de la lista.",
             "VALIDATION_DUPLICATES": "Se eliminaron símbolos duplicados de la lista: {duplicates}.",
             "VALIDATION_SYMBOLS": "Algunos símbolos contienen caracteres inválidos: {invalidos}. Solo se permiten letras (A-Z).",
             "VALIDATION_TOO_MANY": "La lista contiene más de 20 símbolos. Se truncará a los primeros 20.",
@@ -16,7 +15,9 @@ class ErrorManager:
             "EXPORT_EMPTY": "No hay resultados para exportar.",
             "EXPORT_PERMISSION": "No se puede escribir el archivo '{filename}'. Verifique que no esté abierto o que tenga permisos.",
             "EXPORT_UNKNOWN": "Ocurrió un error al exportar el archivo Excel: {error}",
-            "NO_INTERNET": "No se detectó conexión a Internet. Verifique su conexión y vuelva a intentarlo."
+            "NO_INTERNET": "No se detectó conexión a Internet. Verifique su conexión y vuelva a intentarlo.",
+            "API_LIMIT": "Se alcanzó el límite de consultas a la API.",
+            "API_ERROR": "Error general de la API: {0}"
         }
 
 
